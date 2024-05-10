d9a17241eb4f63c6595c74d3fe858f51cebc898c
SayyehBan <sdvp1991david@gmail.com>
2024-05-10 16:39:32 +0330

    Add project files.

---
+++ ProjectIdentityService/Program.cs
@@ -0,0 +1,32 @@
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddIdentityServer().
+    AddDeveloperSigningCredential().
+    AddTestUsers(new List<TestUser> { }).
+    AddInMemoryClients(new List<Client> { }).
+    AddInMemoryIdentityResources(new List<IdentityResource> { });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
